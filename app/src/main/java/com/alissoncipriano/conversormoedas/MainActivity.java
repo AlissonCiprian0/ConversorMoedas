9480fb824b9015db6c4aabf6f2b5ba1812c7c8b9
AlissonCiprian0 <alisson.cipriano@outlook.com>
2022-06-20 00:11:06 -0300

    Hello, world!

--- app/src/main/java/com/alissoncipriano/conversormoedas/MainActivity.java
+++ app/src/main/java/com/alissoncipriano/conversormoedas/MainActivity.java
@@ -3,11 +3,12 @@ package com.alissoncipriano.conversormoedas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
@@ -15,15 +16,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Sem ViewHolder
-//        EditText editValue = findViewById(R.id.edit_value);
-//        editValue.getText();
+//      Sem ViewHolder
+//      EditText editValue = findViewById(R.id.edit_value);
+//      editValue.getText();
 
-//        Com ViewHolder
+//      Com ViewHolder
         this.mViewHolder.editValue       = findViewById(R.id.edit_value);
         this.mViewHolder.textDollar      = findViewById(R.id.text_dollar);
         this.mViewHolder.textEuro        = findViewById(R.id.text_euro);
         this.mViewHolder.buttonCalculate = findViewById(R.id.button_calculate);
+
+        this.mViewHolder.buttonCalculate.setOnClickListener(this);
+//      this.mViewHolder.textEuro.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+//      Verifica se quem chamou o método de Click foi o botão Calcular
+        if (v.getId() == R.id.button_calculate) {
+//          Lógica
+            this.mViewHolder.textDollar.setText("Olá, mundo!");
+        }
     }
 
     private static class ViewHolder {
