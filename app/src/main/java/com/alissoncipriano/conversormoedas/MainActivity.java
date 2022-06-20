309c8eb2d701183992d26836a6ca2081cefe70d9
AlissonCiprian0 <alisson.cipriano@outlook.com>
2022-06-19 23:16:16 -0300

    ViewHolder

--- app/src/main/java/com/alissoncipriano/conversormoedas/MainActivity.java
+++ app/src/main/java/com/alissoncipriano/conversormoedas/MainActivity.java
@@ -3,12 +3,33 @@ package com.alissoncipriano.conversormoedas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Sem ViewHolder
+//        EditText editValue = findViewById(R.id.edit_value);
+//        editValue.getText();
+
+//        Com ViewHolder
+        this.mViewHolder.editValue       = findViewById(R.id.edit_value);
+        this.mViewHolder.textDollar      = findViewById(R.id.text_dollar);
+        this.mViewHolder.textEuro        = findViewById(R.id.text_euro);
+        this.mViewHolder.buttonCalculate = findViewById(R.id.button_calculate);
+    }
+
+    private static class ViewHolder {
+        EditText editValue;
+        TextView textDollar;
+        TextView textEuro;
+        Button buttonCalculate;
     }
 }
